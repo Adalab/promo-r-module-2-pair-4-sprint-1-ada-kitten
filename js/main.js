c7dc25b6f00864da5b1e67ed62a3ffcf09bbdf71
Lara <lrpoblet@gmail.com>
2022-10-19 11:18:35 +0200

    add js variables to cats

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 "use strict";
 
+const kittenImageOne = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
+const kittenNameOne = "Anastacio";
+const kittenDescOne =
+  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenRaceOne = "British Shorthair";
+
+const kittenImageTwo =
+  "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
+const kittenNameTwo = "Fiona";
+const kittenDescTwo = "Cariñoso";
+const kittenRaceTwo = "British Shorthair";
+
+const kittenImageThree =
+  "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
+const kittenNameThree = "Cielo";
+const kittenDescThree = "Antipático";
+const kittenRaceThree = "British Shorthair";
+
 /* const newForm = document.querySelector('.js-new-form');
 
 newForm.classList.remove("collapsed"); */
@@ -10,40 +28,59 @@ const catList = document.querySelector(".js-list");
 Repetir este proceso por cada gatito */
 const kittenOne = ` <li class="card">
 <article>
-  <img class="card_img" src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg" alt="gatito" />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">British Shorthair</h4>
+  <img class="card_img" src=${kittenImageOne} alt="gatito" />
+  <h3 class="card_title">${kittenNameOne.toUpperCase()}</h3>
+  <h4 class="card_race">${kittenRaceOne}</h4>
   <p class="card_description">
-    Ruiseño, juguetón, le guta estar tranquilo y que nadie le
-    moleste. Es una maravilla acariciarle!
+  ${kittenDescOne}
   </p>
 </article>
 </li>`;
 
-const kittenTwo = `<li class="card">
-<img class="card_img"
-  src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" alt="gatito" />
-<h3 class="card_title">Fiona</h3>
-<h4 class="card_race">British Shorthair</h4>
-<p class="card_description">
-  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
-  Es una maravilla acariciarle!
-</p>
+const kittenTwo = ` <li class="card">
+<article>
+  <img class="card_img" src=${kittenImageTwo} alt="gatito" />
+  <h3 class="card_title">${kittenNameTwo.toUpperCase()}</h3>
+  <h4 class="card_race">${kittenRaceTwo}</h4>
+  <p class="card_description">
+  ${kittenDescTwo}
+  </p>
+</article>
 </li>`;
 
-const kittenThree = `<li class="card">
-<img class="card_img"
-  src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"
-  alt="gatito" />
-<h3 class="card_title">Cielo</h3>
-<h4 class="card_race">British Shorthair</h4>
-<p class="card_description">
-  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
-  Es una maravilla acariciarle!
-</p>
+const kittenThree = ` <li class="card">
+<article>
+  <img class="card_img" src=${kittenImageThree} alt="gatito" />
+  <h3 class="card_title">${kittenNameThree.toUpperCase()}</h3>
+  <h4 class="card_race">${kittenRaceThree}</h4>
+  <p class="card_description">
+  ${kittenDescThree}
+  </p>
+</article>
 </li>`;
 
-const content =  kittenOne + kittenTwo +kittenThree;
+const content = kittenOne + kittenTwo + kittenThree;
 
+catList.innerHTML = content;
 
-catList.innerHTML= content;
+const input_search_desc = document.querySelector(".js_in_search_desc");
+
+input_search_desc.value = "Ruiseño";
+const descrSearchText = input_search_desc.value;
+
+input_search_desc.value = "Cariñoso";
+const descrSearchTextTwo = input_search_desc.value;
+
+// input_search_desc.value = "Antipático";
+// const descrSearchTextThree = input_search_desc.value;
+
+if (kittenDescOne.includes(descrSearchText)) {
+  console.log(kittenOne);
+  catList.innerHTML = kittenOne;
+} else {
+  catList.innerHTML = kittenTwo;
+}
+
+// if (kittenDescThree.includes(descrSearchTextThree)) {
+//   catList.innerHTML = kittenThree;
+// }
